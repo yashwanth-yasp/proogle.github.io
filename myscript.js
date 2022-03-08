dff29360eb329c4b4fd0d47757ddfb8850b94fb9
yashwanth-yasp <100475760+yashwanth-yasp@users.noreply.github.com>
2022-03-08 13:23:24 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,29 @@
+﻿function prime() {
+   var number;
+   let flag = 0;
+   number = Number(document.getElementById("input").value);
+   if (number){
+      if (number <= 0){
+         document.getElementById("output").innerHTML = `${number} is netiher a Prime nor Composite number`;
+         return
+      }
+
+      if (number == 1){
+         document.getElementById("output").innerHTML = `${number} is netiher a Prime nor Composite number`;
+         return
+      }
+      for(let i =2; i<number; i++){
+         if (number%i==0){
+            document.getElementById("output").innerHTML = `${number} is a Composite number`;
+            flag = 1;
+         }
+      }
+      if (flag == 0){
+         document.getElementById("output").innerHTML =  `${number} is a Prime number`;
+      }
+   }
+   else{
+      document.getElementById("output").innerHTML =  `It is supposed to be a valid number mate.`;
+   }
+ }
+ 
